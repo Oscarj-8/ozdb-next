20b764ec1716536687ae8061cf42fde42ec6dfde
Abdulahi <mmmabdulahi954@yahoo.com>
2024-06-30 18:05:07 +0300

    Remove '

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,16 +10,16 @@ const AboutPage = () => {
         latest and greatest in cinema from around the world.
       </p>
       <p className="text-lg">
-        Whether you're into action-packed blockbusters, heartwarming dramas, or
-        indie gems, Ozdb has something for everyone. We strive to create a
-        community where movie lovers can come together to share their thoughts,
-        insights, and love for the art of filmmaking.
+        Whether you&apos;re into action-packed blockbusters, heartwarming
+        dramas, or indie gems, Ozdb has something for everyone. We strive to
+        create a community where movie lovers can come together to share their
+        thoughts, insights, and love for the art of filmmaking.
       </p>
       <p className="text-lg">
         Our mission is to make movie discovery easy and enjoyable. With our
-        user-friendly interface and up-to-date content, you'll always be in the
-        know about what's trending in the movie world. Join us on this cinematic
-        journey and immerse yourself in the world of movies with Ozdb.
+        user-friendly interface and up-to-date content, you&apos;ll always be in
+        the know about what&apos;s trending in the movie world. Join us on this
+        cinematic journey and immerse yourself in the world of movies with Ozdb.
       </p>
       <h2 className="text-2xl font-semibold text-sky-600">Our Team</h2>
       <p className="text-lg">
