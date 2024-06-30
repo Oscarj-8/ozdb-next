74e0b39ae37fdca35e8be453a262f2395e4ea2cd
Abdulahi <mmmabdulahi954@yahoo.com>
2024-06-30 12:41:04 +0300

    Add loading effect using nextJs

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <img src="Rolling@1x-1.1s-200px-200px.svg" alt="Loading" />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ const Home = async ({ searchParams }) => {
   const resp = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
 
   const data = await resp.json();
@@ -18,8 +19,6 @@ const Home = async ({ searchParams }) => {
   }
 
   const results = data.results;
-
-  console.log(results.map((item) => item.original_title));
 
   return (
     <div>
