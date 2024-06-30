cd2b8a008d4b54a10fe7b44e921773a87d688d82
Abdulahi <mmmabdulahi954@yahoo.com>
2024-06-30 18:16:06 +0300

    FIx gap issue

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 export default function Movies({ results }) {
   return (
-    <div className="w-full p-3 place-items-center sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-7xl gap-4 mx-auto py-4">
+    <div className="w-full p-3 place-items-center sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-7xl gap-6 space-y-6 md:space-y-0 mx-auto py-4">
       {results.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
