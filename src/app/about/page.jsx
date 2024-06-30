0b47f3fbea086bb617c565e209ce4e93c0db286c
Abdulahi <mmmabdulahi954@yahoo.com>
2024-06-30 10:52:23 +0300

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-4 space-y-4">
+      <h1 className="text-2xl font-medium text-sky-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        voluptate ea quos culpa id, unde pariatur aperiam explicabo
+        exercitationem, provident iusto tenetur. Quas similique ex, quam harum
+        voluptates facere porro.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        voluptate ea quos culpa id, unde pariatur aperiam explicabo
+        exercitationem, provident iusto tenetur. Quas similique ex, quam harum
+        voluptates facere porro.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        voluptate ea quos culpa id, unde pariatur aperiam explicabo
+        exercitationem, provident iusto tenetur. Quas similique ex, quam harum
+        voluptates facere porro.
+      </p>
+    </div>
+  );
+};
+
+export default page;
